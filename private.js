0e96fe54fd40a8a2ac6d063ff111ac15e7c39f93
oZabu <83933546+oZabu@users.noreply.github.com>
2022-03-15 10:55:33 +0900

    Update private.js

--- private.js
+++ private.js
@@ -20,26 +20,37 @@ function timer() {
 
     var _date = nowHour + nowMin;
     var displayTime = document.getElementById("time-display");
+    var alarm = new Audio('Alarm.mp3');
     if (_date >= 945 && _date < 1035) {
         displayTime.style.color = "red";
+        alarm.play();
     } else if (_date >= 1045 && _date < 1135) {
         displayTime.style.color = "red";
+        alarm.play();
     } else if (_date >= 1145 && _date < 1235) {
         displayTime.style.color = "red";
+        alarm.play();
     } else if (_date >= 1235 && _date < 1315) {
         displayTime.style.color = "green";
+        alarm.play();
     } else if (_date >= 1315 && _date < 1405) {
         displayTime.style.color = "red";
+        alarm.play();
     } else if (_date >= 1415 && _date < 1505) {
         displayTime.style.color = "red";
+        alarm.play();
     } else if (_date >= 1515 && _date < 1600) {
         displayTime.style.color = "red";
+        alarm.play();
     } else if (_date >= 1600 && _date < 1605) {
         displayTime.style.color = "orange";
+        alarm.play();
     } else if (_date >= 1605 && _date < 1615) {
         displayTime.style.color = "yellow";
+        alarm.play();
     } else {
         displayTime.style.color = "white";
+        alarm.play();
     };
 
 };
